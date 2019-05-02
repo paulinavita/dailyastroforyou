30be386f6ac50a720ae27c20444a460d7de707b1
yploekito <yp.loekito@gmail.com>
2019-05-02 21:36:37 +0700

    server getvideo enpoint done(?)

--- server/controller/AstroController.js
+++ server/controller/AstroController.js
@@ -1,5 +1,18 @@
-class AstroController {
+const axios = require('axios')
 
+
+class AstroController {
+    static getVideos(req,res){
+        let horoscope = req.query.horoscope
+        axios
+            .get(`https://www.googleapis.com/youtube/v3/search?part=id&q=horoscope May ${horoscope}&type=video`)
+            .then(({data})=>{
+                res.status(200).json(data)
+            })
+            .catch(err =>{
+                res.status(500).json(err)
+            })
+    }
 }
 
 module.exports = AstroController
--- server/routes/astro.js
+++ server/routes/astro.js
@@ -1,5 +1,7 @@
 const router = require("express").Router()
 const AstroController = require('../controller/AstroController')
 
+router.get('/videos', AstroController.getVideos)
+
 
 module.exports = router
