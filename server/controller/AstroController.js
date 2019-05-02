b9a22b697a58759e80f33cbcbc2da3d190ac215a
yploekito <yp.loekito@gmail.com>
2019-05-02 21:48:22 +0700

    additional filter

--- server/controller/AstroController.js
+++ server/controller/AstroController.js
@@ -4,8 +4,9 @@ const axios = require('axios')
 class AstroController {
     static getVideos(req,res){
         let horoscope = req.query.horoscope
+        let month = req.query.month
         axios
-            .get(`https://www.googleapis.com/youtube/v3/search?part=id&q=horoscope May ${horoscope}&type=video`)
+            .get(`https://www.googleapis.com/youtube/v3/search?part=id&q=horoscope ${month} ${horoscope}&type=video&channelId=UCKJ08TgPdcW4as_pfY4Dauw`)
             .then(({data})=>{
                 res.status(200).json(data)
             })
