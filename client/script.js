30b45bebc9e4b93b8b9f60a65e0e732d552bc7a7
yploekito <yp.loekito@gmail.com>
2019-05-03 11:17:40 +0700

    birth validation

--- client/script.js
+++ client/script.js
@@ -3,77 +3,83 @@ const baseURL = `http://localhost:3000`
 
 function getUserZodiac() {
   let birthDate = $('#birthDate').val().split("-")
-  
-  $.ajax({
-    url: `${baseURL}/astros`,
-    method: "POST",
-    data: {
-      birthDate
-    }
-  })
-  .done(data=>{
-    $('#zodiac-name').html(`${data.information.name}`)
-    $('#description-result').html(`${data.dailyPrediction.prediction.personal_life}`);
- 
-     $( "#profession" ).click(function() {
-     $('#description-result').html(`${data.dailyPrediction.prediction.profession}`)
-    });
-
-     $( "#travel" ).click(function() {
-      $('#description-result').html(`${data.dailyPrediction.prediction.travel}`)
-     });
-     $( "#luck" ).click(function() {
-      $('#description-result').html(`${data.dailyPrediction.prediction.luck}`)
-     });
-     $( "#emotions" ).click(function() {
-      $('#description-result').html(`${data.dailyPrediction.prediction.emotions}`)
-     });
-     $( "#health" ).click(function() {
-      $('#description-result').html(`${data.dailyPrediction.prediction.health}`)
-     });
+  let birth = new Date(birthDate) 
+  let timeBirth = birth.getTime()
+  let today = new Date()
+  let timeToday = today.getTime()
+  if( timeBirth > timeToday){
+    Swal.fire('Invalid birthdate')
+  }else{
+    $.ajax({
+        url: `${baseURL}/astros`,
+        method: "POST",
+        data: {
+          birthDate
+        }
+      })
+      .done(data=>{
+        $('#zodiac-name').html(`${data.information.name}`)
+        $('#description-result').html(`${data.dailyPrediction.prediction.personal_life}`);
      
-     let one = ''
-     data.information.physical_traits.forEach((trait,idx) => {
-       one += `${idx+1}. ${trait}\n`
-     })
-     $("#physical-traits").html(one)
-
-     let two = ''
-     data.information.mental_traits.forEach((trait,idx) => {
-      two += `${idx+1}. ${trait}\n`
-    })
-     $("#mental-traits").html(two)
-
-     let three = ''
-     data.information.compatibility.forEach((zod,idx) => {
-      three += `${idx+1}. ${zod}\n`
-    })
-
-     $("#compatibility").html(three)
-
-     let four = ''
-     data.information.how_to_spot.forEach((txt,idx) => {
-      four += `${idx+1}. ${txt}\n`
-    })
-     $("#how-to-spot").html(four)
-
-     let five = ''
-     data.information.hates.forEach((txt,idx) => {
-      five += `${idx+1}. ${txt}\n`
-    })
-     $("#hate").html(five)
-
-     $('#kanan-atas-foto').html('')
-     
-     
-    //  console.log(data.dailyPrediction.sun_sign, 'ini DATA')
-     getVideo(data.dailyPrediction.sun_sign)
-
+         $( "#profession" ).click(function() {
+         $('#description-result').html(`${data.dailyPrediction.prediction.profession}`)
+        });
     
-  })
-  .fail(err=>{
-    console.log(err)
-  })
+         $( "#travel" ).click(function() {
+          $('#description-result').html(`${data.dailyPrediction.prediction.travel}`)
+         });
+         $( "#luck" ).click(function() {
+          $('#description-result').html(`${data.dailyPrediction.prediction.luck}`)
+         });
+         $( "#emotions" ).click(function() {
+          $('#description-result').html(`${data.dailyPrediction.prediction.emotions}`)
+         });
+         $( "#health" ).click(function() {
+          $('#description-result').html(`${data.dailyPrediction.prediction.health}`)
+         });
+         
+         let one = ''
+         data.information.physical_traits.forEach((trait,idx) => {
+           one += `${idx+1}. ${trait}\n`
+         })
+         $("#physical-traits").html(one)
+    
+         let two = ''
+         data.information.mental_traits.forEach((trait,idx) => {
+          two += `${idx+1}. ${trait}\n`
+        })
+         $("#mental-traits").html(two)
+    
+         let three = ''
+         data.information.compatibility.forEach((zod,idx) => {
+          three += `${idx+1}. ${zod}\n`
+        })
+    
+         $("#compatibility").html(three)
+    
+         let four = ''
+         data.information.how_to_spot.forEach((txt,idx) => {
+          four += `${idx+1}. ${txt}\n`
+        })
+         $("#how-to-spot").html(four)
+    
+         let five = ''
+         data.information.hates.forEach((txt,idx) => {
+          five += `${idx+1}. ${txt}\n`
+        })
+         $("#hate").html(five)
+    
+         $('#kanan-atas-foto').html('')
+         
+         
+        //  console.log(data.dailyPrediction.sun_sign, 'ini DATA')
+         getVideo(data.dailyPrediction.sun_sign)
+      })
+      .fail(err=>{
+        console.log(err)
+      })
+  }
+  
 }
 
 function generateDailyTarot() {
